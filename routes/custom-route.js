adb9876242bb89f805bd6fc5909d0a808fd5afa6
Lars Henrik Nordli <larshenrik91@gmail.com>
2017-01-25 11:22:39 +0100

    Added more example code

--- routes/custom-route.js
+++ routes/custom-route.js
@@ -1,28 +1,69 @@
 // custom-route.js
 // Route to do custom stuff
+
 // Reached on /api/custom-route
+var route = '/api/custom-route';
+
+//Change variable name and model file name to reflect the Watson Service
+var WatsonService = require('../models/custom-model');
 
 var express = require('express');
 var router = express.Router();
 
 //POST something
-router.post('/', function(req, res, next) {
+router.post('/' + route, function(req, res, next) {
+  var action = WatsonService.apiFunction();
+
   var reqBody = req.body;
+
+  var data = {
+    "action": "POST",
+    "status": "Successfully posted"
+  };
+
+  res.status(200).json(data);
 });
 
 //GET something
-router.get('/', function(req, res, next) {
+router.get('/' + route, function(req, res, next) {
+  var action = WatsonService.apiFunction();
+
   var reqBody = req.body;
+
+  var data = {
+    "action": "GET",
+    "status": "Successfully gotten"
+  };
+
+  res.status(200).json(data);
 });
 
 //PUT something
-router.put('/', function(req, res, next) {
+router.put('/' + route, function(req, res, next) {
+  var action = WatsonService.apiFunction();
+
   var reqBody = req.body;
+
+  var data = {
+    "action": "PUT",
+    "status": "Successfully put"
+  };
+
+  res.status(200).json(data);
 });
 
 //DELETE something
-router.delete('/', function(req, res, next) {
+router.delete('/' + route, function(req, res, next) {
+  var action = WatsonService.apiFunction();
+
   var reqBody = req.body;
+
+  var data = {
+    "action": "DELETE",
+    "status": "Successfully deleted"
+  };
+
+  res.status(200).json(data);
 });
 
 module.exports = router;
